5faed9a1b90bba9558ee29eb7a72b5f1bf3a3bf8
fazalshahzad <shancompany322@gmail.com>
2022-05-26 16:48:00 +0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { DetailComponent } from './detail/detail.component';
+import { FeatureComponent } from './feature/feature.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { TeamComponent } from './team/team.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: 'feature', component: FeatureComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'test', component: TestComponent }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { DetailComponent } from './detail/detail.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { TeamComponent } from './team/team.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    DetailComponent,
+    FeatureComponent,
+    ProductComponent,
+    ServiceComponent,
+    TeamComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
